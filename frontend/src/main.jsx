9bd7e7491bdc24a78aeaea2273940825463d5f0c
logic-automation <andrew@logicautomationllc.com>
2024-01-15 17:58:06 -0800

    demo working

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,12 +8,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import PageNotFound from "./404Page.jsx";
 import './index.css'
+import Index from './Brian.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Router>
       <Routes>
-        <Route exact path="/" element={<App />} />
+        <Route exact path="/" element={<Index />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </Router>
